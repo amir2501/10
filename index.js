39e7859ab00cb8865c3c2897738cb5aabd29a7ee
Erkinov Amirbek <erkinovamir100@gmail.com>
2024-10-07 11:58:19 +0500

    bug Fix: fixed bug with not removing background color of button in mobile version

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-let id_text = document.getElementById('id');
-let advice_text = document.getElementById('advice');
-let btn = document.getElementById('btn');
+const id_text = document.getElementById("id");
+const advice_text = document.getElementById("advice");
+const btn = document.getElementById("btn");
+const width = window.innerWidth;
 
+btn.removeAttribute("href");
 
 function get_advice() {
-    id_text.textContent = "";
-    advice_text.textContent = `Loading`;
-    fetch("https://api.adviceslip.com/advice").then(res => res.json()).then(function (data) {
-        let {id, advice} = data.slip;
-        id_text.textContent = id;
-        advice_text.textContent = `"${advice}"`;
-    })
+  id_text.textContent = "";
+  advice_text.textContent = `Loading`;
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then(function (data) {
+      let { id, advice } = data.slip;
+      id_text.textContent = id;
+      advice_text.textContent = `"${advice}"`;
+    });
 
+  if (width < 800) {
+    btn.style.boxShadow = "0 0 20px hsl(150, 100%, 66%)";
+    btn.style.scale = "0.9";
+
+    setTimeout(() => {
+      btn.style.boxShadow = "none";
+      btn.style.scale = "1";
+    }, 300);
+  }
 }
 
 get_advice();
 
-btn.addEventListener('click', get_advice)
-
-
-
-
-
+btn.addEventListener("click", get_advice);
